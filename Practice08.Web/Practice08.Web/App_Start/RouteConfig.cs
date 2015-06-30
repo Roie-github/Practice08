7c71d9d11ba7549ce63cc1c4b5297e6a236e4541
Roie <sup3rmn_21@yahoo.com>
2015-06-30 10:22:41 +0800

    5th

--- Practice08.Web/Practice08.Web/App_Start/RouteConfig.cs
+++ Practice08.Web/Practice08.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Practice08.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
